a10d74a8573eff96dda70233c827efae8ebc0647
hussein <hussein66253@icloud.com>
2020-02-05 09:29:14 +0200

    Add t 3 dynamic components and updated

--- js/add-content.js
+++ js/add-content.js
@@ -1,18 +1,9 @@
-// var today= new Date();
-// var hourNow= today.getHours();
-// var greeting;
 var name = prompt("Lets starat to know each other we are Constructive & CO and you are ?");
 var age = prompt("We welcom to all people on our site and we like to know your age ?");
-var gender = prompt("We want to if you ar male or female to helb to cotact us ?");
-var Msg; 
+var gender = prompt("We want to if you are male or female to help us to cotact you ?");
+var result= confirm("Are you ready to see what make us the best!");
 
-if (gender == male) {
-    greeting = 'Good evening, Class!';
-} else if  (hourNow > 12) {
-    greeting = 'Good after noon, Class!';
-}else if(hourNow >= 0) {
-    greeting ='Good morning!';
-} else {
-    greeting = 'Something went wrong!';
-} 
-document.write('<h3>'+greeting+'</h3>');
+if(result==true){alert("lets get start.")}
+else{alert("okay,maybe late")
+window.close();
+}
